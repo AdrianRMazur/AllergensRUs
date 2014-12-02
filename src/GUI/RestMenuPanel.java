a431fdd56952fde41bea66ae0370ac28e21f8f07
AdrianRMazur <adrianmaz@live.com>
2014-12-01 23:55:42 -0500

    serves DB works

--- src/GUI/RestMenuPanel.java
+++ src/GUI/RestMenuPanel.java
@@ -16,22 +16,25 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-//import org.h2.tools.DeleteDbFiles;
+import org.h2.tools.DeleteDbFiles;
 
 
 // regular menu
+
+
 
 public class RestMenuPanel extends JFrame{
 
 	
 	private JButton back; 
+	private String restaurantName; 
+	private Object[][] data; 
+	
 	
 	public RestMenuPanel(String title) {
-		
 		super(title);
-		
+		restaurantName=FirstPanel.restaurant.getText(); 
 		builder();
-		
 		back.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				dispose();
@@ -42,33 +45,17 @@ public class RestMenuPanel extends JFrame{
 	
 	private void builder(){
 		
-		//try {
-			//DBQuery();
-	//	} catch (ClassNotFoundException e) {
+		try {
+			DBQuery();
+		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//} catch (SQLException e) {
-			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//} 
+			e.printStackTrace();
+		} 
 		back = new JButton ("Back");
 		
-		String[] columnNames = {"Dish",
-                "Price",
-                "Weekday"};
+		String[] columnNames = {"Dish", "Day"};
 		
-		 Object[][] data = {
-			        {"Kathy", "Smith",
-			         "Snowboarding"},
-			        {"John", "Doe",
-			         "Rowing"},
-			        {"Sue", "Black",
-			         "Knitting"},
-			        {"Jane", "White",
-			         "Speed reading"},
-			        {"Joe", "Brown",
-			         "Pool"}
-			        };
+
 		 
 		 final JTable table = new JTable(data, columnNames);
 		 table.setPreferredScrollableViewportSize(new Dimension(300, 70));
@@ -88,7 +75,7 @@ public class RestMenuPanel extends JFrame{
 		
 	}
 	
-	/*
+	
 	
 	public String DBQuery() throws SQLException, ClassNotFoundException{
 		
@@ -100,14 +87,31 @@ public class RestMenuPanel extends JFrame{
 
         // this line would initialize the database
         // from the SQL script file 'init.sql'
-        // stat.execute("runscript from 'init.sql'");
+        //stat.execute("runscript from 'init.sql'");
 
-       // stat.execute("create table test(id int primary key, name varchar(255))");
-        //stat.execute("insert into test values(1, 'Hello')");
+       //stat.execute("create table test(id int primary key, name varchar(255))");
+       // stat.execute("insert into test values(1, 'Hello')");
         ResultSet rs;
-        rs = stat.executeQuery("select * from Customers");
+        ResultSet count;
+        count = stat.executeQuery("Select Count(*) From Serves Where Restaurant = '"+restaurantName+"'");
+        count.next();
+        int size= Integer.parseInt(count.getString("Count(*)"));
+        data= new Object[size][2];
+        //System.out.println("X: " + count.getString("Count(*)"));
+        rs = stat.executeQuery("Select * From Serves Where Restaurant = '"+restaurantName+"'");
+        int c =0; 
+        //System.out.println("X: " + data.length );
         while (rs.next()) {
-            System.out.println(rs.getString("name"));
+        	data[c][0] = rs.getString("Food");
+        	data[c][1] = rs.getString("Day");
+            /*System.out.print(rs.getString("Restaurant"));
+            System.out.print(" ");
+            System.out.print(rs.getString("Food"));
+            System.out.print(" ");
+            System.out.print(rs.getString("Day"));
+            System.out.println("");
+            */
+            c++;
         }
         stat.close();
         conn.close();
@@ -117,7 +121,7 @@ public class RestMenuPanel extends JFrame{
 		
 	}
 	
-	*/
+	
 	
 
 }
