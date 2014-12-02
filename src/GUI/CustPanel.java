090313044b07e6cb1e4a670d8b35d050daf28563
AdrianRMazur <adrianmaz@live.com>
2014-12-02 01:42:11 -0500

    looking into querys

--- src/GUI/CustPanel.java
+++ src/GUI/CustPanel.java
@@ -3,6 +3,11 @@ package GUI;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -14,6 +19,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+
+import org.h2.tools.DeleteDbFiles;
 
 
 
@@ -77,5 +84,43 @@ public class CustPanel extends JFrame {
 		
 	
 	}
+	
+public void DBQuery() throws SQLException, ClassNotFoundException{
+		
+
+        DeleteDbFiles.execute("~", "test", true);
+
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+        Statement stat = conn.createStatement();
+        // this line would initialize the database
+        // from the SQL script file 'init.sql'
+        //stat.execute("runscript from 'init.sql'");
+
+        String current = System.getProperty("user.dir");
+       // System.out.println("Current working directory in Java : " + current);
+
+        
+       stat.execute("DROP TABLE IF EXISTS SERVES; create table Serves As Select * from csvread('"+current+"\\data\\Serves1.0.csv')");
+       stat.execute("DROP TABLE IF EXISTS Allergens; create table Allergens As Select * from csvread('"+current+"\\data\\Allergens1.5.csv')");
+        
+       //stat.execute("create table test(id int primary key, name varchar(255))");
+       // stat.execute("insert into test values(1, 'Hello')");
+        ResultSet rs;
+    
+        //System.out.println("X: " + count.getString("Count(*)"));
+        rs = stat.executeQuery("Select Distinct(Restaurant) From Serves");
+        int c =0; 
+        //System.out.println("X: " + data.length );
+        while (rs.next()) {
+        	
+           
+            c++;
+        }
+        stat.close();
+        conn.close();
+		
+	}
+	
 	
 }
