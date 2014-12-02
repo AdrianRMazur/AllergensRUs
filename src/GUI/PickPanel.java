c0ca310efa91713f2ec83e9a1b9e0f6ba4ef76e5
Kosti Papajani <kostipapajani@gmail.com>
2014-12-02 00:19:03 -0500

    more changes

--- src/GUI/PickPanel.java
+++ src/GUI/PickPanel.java
@@ -1,5 +1,11 @@
 package GUI;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
@@ -10,8 +16,13 @@ import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import org.h2.tools.DeleteDbFiles;
+
 public class PickPanel extends JFrame{
 
+	private DefaultListModel <String>listModel = new DefaultListModel<String>();
+	private JList list = new JList<String>(listModel);
+	
 	public PickPanel(String title) {
 		super(title);
 		
@@ -20,7 +31,7 @@ public class PickPanel extends JFrame{
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setSelectedIndex(0);
 		list.setVisibleRowCount(5);
-		list.setSize(10, 5);
+		//list.setSize(10, 5);
 		JScrollPane listscroller = new JScrollPane(list);
 
 		JPanel finalpanel = new JPanel ();
@@ -35,6 +46,46 @@ public class PickPanel extends JFrame{
 				dispose();
 			}			
 		});
+	}
+	
+	
+	
+	public void DBQuery() throws SQLException, ClassNotFoundException{
+		
+
+        DeleteDbFiles.execute("~", "test", true);
+
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+        Statement stat = conn.createStatement();
+
+        // this line would initialize the database
+        // from the SQL script file 'init.sql'
+        //stat.execute("runscript from 'init.sql'");
+
+       //stat.execute("create table test(id int primary key, name varchar(255))");
+       // stat.execute("insert into test values(1, 'Hello')");
+        ResultSet rs;
+    
+        //System.out.println("X: " + count.getString("Count(*)"));
+        rs = stat.executeQuery("Select Restaurant From Serves");
+        int c =0; 
+        //System.out.println("X: " + data.length );
+        while (rs.next()) {
+        	
+        	listModel.addElement(rs.getString("Restaurant"));
+            /*System.out.print(rs.getString("Restaurant"));
+            System.out.print(" ");
+            System.out.print(rs.getString("Food"));
+            System.out.print(" ");
+            System.out.print(rs.getString("Day"));
+            System.out.println("");
+            */
+            c++;
+        }
+        stat.close();
+        conn.close();
+		
 	}
 
 }
